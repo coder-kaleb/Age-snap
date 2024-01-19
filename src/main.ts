425d42f0647e53f219377c0cc96bffcdc3adc655
Kaleb Mikiyas <starbouns76@gmail.com>
2024-01-20 02:20:27 +0300

    main.ts module note found is resolved

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-let na = "kaleb";
+let yearEl = <HTMLInputElement>document.getElementById("year");
+let monthEl = <HTMLInputElement>document.getElementById("month");
+let dayEl = <HTMLInputElement>document.getElementById("day");
+let btnSubmit = <HTMLButtonElement>document.getElementById("btn-submit");
+
+// error Elements
+let errDay = document.getElementById('error-day')
+let errMonth = <HTMLElement>document.getElementById('error-month')
+let errYear = document.getElementById('error-year')
+
+
+const calculateAge = (e: Event) => {
+    e.preventDefault()
+    if(+monthEl.value > 12) {
+        // errMonth?.textContent = 'Must be vaild month'
+    }
+    
+};
+btnSubmit?.addEventListener("click", calculateAge);
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         poppins: "'Poppins', sans-serif",
       },
+      screens: {
+        'mobile': '550px'
+      },
+      colors: {
+        'purple': 'hsl(259, 100%, 65%)',
+        'light-red': 'hsl(0, 100%, 67%)' 
+      }
     },
   },
   plugins: [],
